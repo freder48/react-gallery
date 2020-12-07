2a9fbe1e7d67a51956d8fb17d46deaf6e7a30a2f
Josie Fredericksen <jfredericksen12@gmail.com>
2020-12-07 09:45:31 -0600

    updated sql file inserts

--- database.sql
+++ database.sql
@@ -6,6 +6,20 @@ CREATE TABLE "gallery" (
   "likes" INTEGER DEFAULT 0
 );
 
-INSERT INTO gallery (path, alt, description, likes)
-VALUES ('images/books.jpeg', 'Manta Ray', 'Photo of a manta ray from scuba diving in Indonesia', 0);
 
+INSERT INTO gallery (path, alt, description, likes)
+VALUES ('images/books.jpeg', 'Manta Ray', 'Photo of the stack of books next to my bed', 0);
+INSERT INTO gallery (path, alt, description, likes)
+VALUES ('images/SanAndres.jpg', 'San Andres Island', 'Photo of San Andres Island near Nicaragua', 0);
+INSERT INTO gallery (path, alt, description, likes)
+VALUES ('images/Hazel.jpg', 'Hazel the dog', 'Photo of the love of my life', 0);
+INSERT INTO gallery (path, alt, description, likes)
+VALUES ('images/friends.jpeg', 'Photo of friends', 'Photo of my friends and I camping at Big Bear Lake, CA', 0);
+INSERT INTO gallery (path, alt, description, likes)
+VALUES ('images/mantaRay.jpg', 'Manta Ray', 'Photo of a manta ray from scuba diving in Indonesia', 0);
+INSERT INTO gallery (path, alt, description, likes)
+VALUES ('images/garden.jpg', 'Garden', 'Photo of my quarantine gardens', 0);
+INSERT INTO gallery (path, alt, description, likes)
+VALUES ('images/yoga.JPG', 'Yoga', 'Photo of me doing aerial yoga in Ecuador', 0);
+INSERT INTO gallery (path, alt, description, likes)
+VALUES ('images/pretzel.jpg', 'Homemade Pretzel', 'Photo of pretzels I made!', 0);
